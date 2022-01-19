6936b42977a888bf80ef158ffa1ea905d7754d7f
aaryashaji1 <aaryashajit@gmail.com>
2022-01-18 23:23:25 -0600

    Add project files.

---
+++ PMTProgram/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace PMTProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.txtNop = new System.Windows.Forms.TextBox();
+            this.txtInterest = new System.Windows.Forms.TextBox();
+            this.txtPayments = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRINCIPAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUMBER OF PAYMENTS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "INTEREST RATE PER PAYMENT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(169, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PMT";
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Location = new System.Drawing.Point(373, 98);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(128, 20);
+            this.txtPrincipal.TabIndex = 4;
+            // 
+            // txtNop
+            // 
+            this.txtNop.Location = new System.Drawing.Point(373, 145);
+            this.txtNop.Name = "txtNop";
+            this.txtNop.Size = new System.Drawing.Size(128, 20);
+            this.txtNop.TabIndex = 5;
+            // 
+            // txtInterest
+            // 
+            this.txtInterest.Location = new System.Drawing.Point(373, 195);
+            this.txtInterest.Name = "txtInterest";
+            this.txtInterest.Size = new System.Drawing.Size(128, 20);
+            this.txtInterest.TabIndex = 6;
+            // 
+            // txtPayments
+            // 
+            this.txtPayments.Location = new System.Drawing.Point(373, 310);
+            this.txtPayments.Name = "txtPayments";
+            this.txtPayments.Size = new System.Drawing.Size(128, 20);
+            this.txtPayments.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Location = new System.Drawing.Point(251, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CALCULATE PMT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(655, 397);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPayments);
+            this.Controls.Add(this.txtInterest);
+            this.Controls.Add(this.txtNop);
+            this.Controls.Add(this.txtPrincipal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.TextBox txtNop;
+        private System.Windows.Forms.TextBox txtInterest;
+        private System.Windows.Forms.TextBox txtPayments;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PMTProgram/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMTProgram
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            
+             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double principle = double.Parse(txtPrincipal.Text);
+            double numberofpayments = double.Parse(txtNop.Text);
+            double interest = double.Parse(txtInterest.Text);
+            double pmt = -interest * (principle * Math.Pow(1 + interest, numberofpayments)) / ((Math.Pow(1 + interest, numberofpayments) - 1));
+            txtPayments.Text = (-pmt).ToString();
+
+
+        }
+    }
+}
